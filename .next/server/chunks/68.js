2b081ade6d5043e0d0f806eae18c8d2c847dfbd8
ZhiYong Jie <823425525@qq.com>
2024-07-13 20:43:37 +0800

    feat: Deploy Nextra site to docs directory

--- .next/server/chunks/68.js
+++ .next/server/chunks/68.js
@@ -6381,7 +6381,7 @@ Object.defineProperty(exports, "__esModule", ({
 exports.addBasePath = addBasePath;
 var _addPathPrefix = __webpack_require__(1751);
 var _normalizeTrailingSlash = __webpack_require__(9819);
-const basePath =  false || "";
+const basePath = "/jiezhiyong.github.io" || 0;
 function addBasePath(path, required) {
     if (false) {}
     return (0, _normalizeTrailingSlash).normalizePathTrailingSlash((0, _addPathPrefix).addPathPrefix(path, basePath));
@@ -6456,7 +6456,7 @@ Object.defineProperty(exports, "__esModule", ({
     value: true
 }));
 exports.getDomainLocale = getDomainLocale;
-const basePath = (/* unused pure expression or super */ null && ( false || ""));
+const basePath = (/* unused pure expression or super */ null && ("/jiezhiyong.github.io" || 0));
 function getDomainLocale(path, locale, locales, domainLocales) {
     if (false) {} else {
         return false;
@@ -6483,7 +6483,7 @@ Object.defineProperty(exports, "__esModule", ({
 }));
 exports.hasBasePath = hasBasePath;
 var _pathHasPrefix = __webpack_require__(4567);
-const basePath =  false || "";
+const basePath = "/jiezhiyong.github.io" || 0;
 function hasBasePath(path) {
     return (0, _pathHasPrefix).pathHasPrefix(path, basePath);
 }
@@ -6652,7 +6652,7 @@ var _imageConfig = __webpack_require__(5843);
 var _imageConfigContext = __webpack_require__(744);
 var _warnOnce = __webpack_require__(618);
 var _imageLoader = _interop_require_default(__webpack_require__(9552));
-const configEnv = {"deviceSizes":[640,750,828,1080,1200,1920,2048,3840],"imageSizes":[16,32,48,64,96,128,256,384],"path":"/_next/image","loader":"default","dangerouslyAllowSVG":false,"unoptimized":true};
+const configEnv = {"deviceSizes":[640,750,828,1080,1200,1920,2048,3840],"imageSizes":[16,32,48,64,96,128,256,384],"path":"/jiezhiyong.github.io/_next/image","loader":"default","dangerouslyAllowSVG":false,"unoptimized":true};
 const allImgs = new Map();
 let perfObserver;
 if (true) {
@@ -7446,7 +7446,7 @@ Object.defineProperty(exports, "__esModule", ({
 }));
 exports.removeBasePath = removeBasePath;
 var _hasBasePath = __webpack_require__(3933);
-const basePath =  false || "";
+const basePath = "/jiezhiyong.github.io" || 0;
 function removeBasePath(path) {
     if (false) {}
     path = path.slice(basePath.length);
@@ -9703,7 +9703,7 @@ class Router {
         // if auto prerendered and dynamic route wait to update asPath
         // until after mount to prevent hydration mismatch
         const autoExportDynamic = (0, _isDynamic).isDynamicRoute(pathname1) && self.__NEXT_DATA__.autoExport;
-        this.basePath =  false || "";
+        this.basePath = "/jiezhiyong.github.io" || 0;
         this.sub = subscription;
         this.clc = null;
         this._wrapApp = wrapApp;
